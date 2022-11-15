e77b226f377ad64f1e00550693837b066c475874
FilipiLuvatos <37461362+FilipiLuvatos@users.noreply.github.com>
2022-11-15 18:55:08 -0300

    Estilizando pagina

---
+++ src/app/nova_transferencia.component.ts
@@ -0,0 +1,11 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-nova_transferencia',
+    templateUrl:'./nova_transferencia.component.html',
+    styleUrls:['./nova_transferencia.component.scss']
+})
+export class NovaTransferenciaComponent{
+
+}
